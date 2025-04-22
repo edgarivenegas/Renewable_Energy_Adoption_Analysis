2f6f7e72678391a11cb7d72900ba7ae3fed60e7d
Edgar Venegas <edgarivenegas@gmail.com>
2025-04-22 01:33:06 -0400

    Add files via upload

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .data_summary import summarize_data, preprocess_pipeline, train_test_splits, linear_reg_pred
+from .visualization import plot_linechart, plot_correlations, plot_rsquared, plot_act_v_pred, plot_residuals
+from .model_metrics import eval_model
